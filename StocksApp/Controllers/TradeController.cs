4c512f0081e054ad2df3999a854d5d0cb4948d44
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-03-04 19:39:37 +0200

    feat(SA-3): orders page & buyOrder, sellOrder functionality

--- StocksApp/Controllers/TradeController.cs
+++ StocksApp/Controllers/TradeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using StocksApp.ServiceContracts;
+using StocksApp.ServiceContracts.DTO;
 using StocksApp.UI;
 using StocksApp.UI.Models;
 
@@ -22,9 +23,6 @@ namespace StocksApp.Controllers
             _stocksService = stocksService;
         }
 
-        [Route("/")]
-        [Route("[action]")]
-        [Route("~/[controller]")]
         public async Task<IActionResult> IndexAsync()
         {
             //reset stock symbol if not exists
@@ -45,10 +43,12 @@ namespace StocksApp.Controllers
             //load data from finnHubService into model object
             if (companyProfileDictionary != null && stockQuoteDictionary != null)
             {
-                stockTrade = new StockTrade() { 
-                    StockSymbol = Convert.ToString(companyProfileDictionary["ticker"]), 
-                    StockName = Convert.ToString(companyProfileDictionary["name"]), 
-                    Price = Convert.ToDouble(stockQuoteDictionary["c"].ToString()) };
+                stockTrade = new StockTrade()
+                {
+                    StockSymbol = Convert.ToString(companyProfileDictionary["ticker"]),
+                    StockName = Convert.ToString(companyProfileDictionary["name"]),
+                    Price = Convert.ToDouble(stockQuoteDictionary["c"].ToString())
+                };
             }
 
             //Send Finnhub token to view
@@ -57,5 +57,59 @@ namespace StocksApp.Controllers
             return View(stockTrade);
         }
 
+        public IActionResult Orders()
+        {
+            List<BuyOrderResponse> buyOrderResponses = _stocksService.GetBuyOrders();
+            List<SellOrderResponse> sellOrderResponses = _stocksService.GetSellOrders();
+            ViewBag.TradingOptions = _tradingOptions;
+            return View(new Orders { BuyOrders = buyOrderResponses,SellOrders = sellOrderResponses });
+        }
+
+        [HttpPost]
+        public IActionResult SellOrder(SellOrderRequest sellOrderRequest)
+        {
+            //update date of order
+            sellOrderRequest.DateTimeOfOrder = DateTime.Now;
+
+            //re-validate the model object after updating the date
+            ModelState.Clear();
+            TryValidateModel(sellOrderRequest);
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                StockTrade stockTrade = new StockTrade() { StockName = sellOrderRequest.StockName, Quantity = sellOrderRequest.Quantity, StockSymbol = sellOrderRequest.StockSymbol };
+                return View("Index", stockTrade);
+            }
+
+            //invoke service method
+            SellOrderResponse sellOrderResponse = _stocksService.CreateSellOrder(sellOrderRequest);
+
+            return RedirectToAction(nameof(Orders));
+        }
+
+        [HttpPost]
+        public IActionResult BuyOrder(BuyOrderRequest buyOrderRequest)
+        {
+            //update date of order
+            buyOrderRequest.DateTimeOfOrder = DateTime.Now;
+
+            //re-validate the model object after updating the date
+            ModelState.Clear();
+            TryValidateModel(buyOrderRequest);
+
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                StockTrade stockTrade = new StockTrade() { StockName = buyOrderRequest.StockName, Quantity = buyOrderRequest.Quantity, StockSymbol = buyOrderRequest.StockSymbol };
+                return View("Index", stockTrade);
+            }
+
+            //invoke service method
+            BuyOrderResponse buyOrderResponse = _stocksService.CreateBuyOrder(buyOrderRequest);
+
+            return RedirectToAction(nameof(Orders));
+        }
     }
 }
