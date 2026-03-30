b01d849e6a70c6f3f775fe2622f9ef3253144f20
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-03-30 20:58:47 +0300

    feat(SA-8): add action filter

---
+++ StocksApp.ServiceContracts/DTO/IOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace StocksApp.ServiceContracts.DTO
+{
+    public interface IOrderRequest
+    {
+        public string StockSymbol { get; set; }
+        public string StockName { get; set; }
+        public DateTime DateTimeOfOrder { get; set; }
+        public uint Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
